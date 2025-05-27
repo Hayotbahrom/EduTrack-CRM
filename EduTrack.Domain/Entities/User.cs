2faed4147ecb3d00b4f40a946ad0821f82a3ee40
Hayotbahrom Nurdinov <khayotbakhrom_nurdinov@epam.com>
2025-05-27 10:07:09 +0500

    added user entity

---
+++ EduTrack.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using EduTrack.Domain.Commons;
+
+namespace EduTrack.Domain.Entities
+{
+    public class User : Auditable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
