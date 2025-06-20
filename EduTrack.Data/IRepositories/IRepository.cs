91e9a5686406d9c1d1d9f48ff4a4eab809e12de4
Hayotbahrom Nurdinov <khayotbakhrom_nurdinov@epam.com>
2025-06-20 16:04:39 +0500

    created secondmigration, generic repository

---
+++ EduTrack.Data/IRepositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using EduTrack.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduTrack.Data.IRepositories
+{
+    internal interface IRepository<TEntity> where TEntity : Auditable
+    {
+        Task<bool> DeleteAsync(long id);
+        IQueryable<TEntity> SelectAll();
+        Task<TEntity> SelectByIdAsync(long id);
+        Task<TEntity> InsertAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+    }
+}
