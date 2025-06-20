1f4bf6fd9553fe53e8b21e74f7d67b577d0ae80b
Hayotbahrom Nurdinov <khayotbakhrom_nurdinov@epam.com>
2025-06-20 15:52:16 +0500

    first migration

--- EduTrack.Domain/Entities/Student.cs
+++ EduTrack.Domain/Entities/Student.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EduTrack.Domain.Commons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace EduTrack.Domain.Entities
 {
-    public class Student
+    public class Student : Auditable
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
