394c7f7fca51dc692e5cc311c0d06c560b566558
Xayotbaxrom.Nurdinov <hayotbahrom2002@gmail.com>
2025-12-01 13:41:58 +0500

    updated setting

--- src/EduTrack.Domain/Entities/Attendance.cs
+++ src/EduTrack.Domain/Entities/Attendance.cs
@@ -1,22 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace EduTrack.Domain.Entities;
 
-namespace EduTrack.Domain.Entities
+public class Attendance
 {
-    public class Attendance 
-    {
-        public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public bool IsPresent { get; set; }
-        public string Remarks { get; set; }
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public bool IsPresent { get; set; }
+    public string Remarks { get; set; }
 
-        public int StudentId { get; set; }
-        public Student Student { get; set; }
+    public int StudentId { get; set; }
+    public Student Student { get; set; }
 
-        public int GroupId { get; set; }
-        public Group Group { get; set; }
-    }
+    public int GroupId { get; set; }
+    public Group Group { get; set; }
 }
