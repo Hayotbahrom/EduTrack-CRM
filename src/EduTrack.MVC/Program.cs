a84d9758a8118428cdf7641b278f36a365d5d638
david fergana <hayotbahrom2002@gmail.com>
2026-08-24 11:43:23 +0500

    added auto migration when project build

--- src/EduTrack.MVC/Program.cs
+++ src/EduTrack.MVC/Program.cs
@@ -35,4 +35,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<EduDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
